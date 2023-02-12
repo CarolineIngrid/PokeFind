5449a200a3866216fb9410bc3c632bbd7284b8d5
Ingrid Caroline <ingridcaroline3012@gmail.com>
2023-02-12 20:39:36 -0300

    Aplica background color e fonte

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,31 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <h1> POKEMON </h1>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
     //
   }),
 };
 </script>
+
+<style>
+#app{
+  background: #e7e7ac;
+	background: linear-gradient(to right, rgb(214, 197, 236), rgb(160, 203, 230));
+	color: white;
+	margin: 0;
+	/* font-family: rubik; */
+}
+
+/* ---------- TIPOGRAFIA --------- */
+h1{
+  color: yellow;
+  -webkit-text-stroke: 2px #000;
+}
+</style>
