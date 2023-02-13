bd351ea14e5147ecd2bec970def076cc4a6bb13d
Ingrid Caroline <ingridcaroline3012@gmail.com>
2023-02-13 13:20:51 -0300

    Adiciona imagens respectivas aos pokemons

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,43 @@
     <h1> POKEMON </h1>
 
     <v-responsive
-    class="md-auto px-12 py-6"
-    >
-    <v-text-field
-      label="First name"
-      hide-details="auto"
-      solo
-    ></v-text-field>
-  </v-responsive>
-      <v-container>
+      class="md-auto px-12 py-6"
+      >
+      <v-text-field
+        label="Pesquise"
+        hide-details="auto"
+        solo>
+      </v-text-field>
+    </v-responsive>
+
+    <v-container>
+      <v-card>
+        <!-- {{ pokemons }} -->
+        <v-row>
+        <v-col
+          cols="3"
+          v-for="pokemon in pokemons.slice(0,12)"
+          :key="pokemon.name"
+        >
         <v-card>
-          {{ pokemons }}
+          <img 
+          :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(
+            pokemon
+          )}.png`"
+          :alt="pokemon.name"
+          width="60%">
+          
+          <h2 class="text-center">{{ pokemon.name }}</h2>
+          <!-- <PokemonCard :pokemon="pokemon" @clicked="show_pokemon" /> -->
         </v-card>
-      </v-container>
+        
+          
+        </v-col>
+      </v-row>
+      </v-card>
+
+    </v-container>
+
   </v-app>
 </template>
 
@@ -36,9 +60,14 @@ export default {
   },
 
   mounted (){
-    axios.get("https://pokeapi.co/api/v2/pokemon?limit=10").then((response) => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0").then((response) => {
       this.pokemons = response.data.results;
     });
+  },
+  methods: {
+    get_id(pokemon){
+      return pokemon.url.split("/")[6];
+    }
   },
 };
 </script>
